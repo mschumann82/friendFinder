c80d257cfa2186145122b648a3684c83fe9cb886
Michael Schumann <schumann82@gmail.com>
2018-07-29 14:51:56 -0700

    updated

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -19,4 +19,8 @@ router.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
+  router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
   module.exports = router;
